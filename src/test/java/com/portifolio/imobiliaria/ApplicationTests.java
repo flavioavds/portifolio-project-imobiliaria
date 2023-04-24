fb1deda49f5a10db91a3a90accd805d1f4e2b122
Flavio <suporte.flavio.souza@hotmail.com>
2023-04-24 19:13:22 -0300

    create project

---
+++ src/test/java/com/portifolio/imobiliaria/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
