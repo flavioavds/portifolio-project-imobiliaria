ba1f6cbfd8526883ad6a186c8b2162bc184ecd78
Flavio <suporte.flavio.souza@hotmail.com>
2023-05-17 14:55:59 -0300

    End Points Physical Person Atualizado e Rotas Completas

--- src/main/java/com/portifolio/imobiliaria/controllers/PhysicalPersonController.java
+++ src/main/java/com/portifolio/imobiliaria/controllers/PhysicalPersonController.java
@@ -1,17 +1,24 @@
 package com.portifolio.imobiliaria.controllers;
 
+import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portifolio.imobiliaria.dtos.person.CpfDTORequest;
@@ -20,6 +27,7 @@ import com.portifolio.imobiliaria.events.OnRegistrationSuccessEvent;
 import com.portifolio.imobiliaria.service.PhysicalPersonService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/physical-persons")
@@ -48,6 +56,43 @@ public class PhysicalPersonController {
     public ResponseEntity<Page<CpfDTOResponse>> getAllPhysicalPersons(Pageable pageable) {
         Page<CpfDTOResponse> physicalPersonPage = service.findAll(pageable);
         return ResponseEntity.ok(physicalPersonPage);
+    }
+	
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<CpfDTOResponse> getById(@PathVariable UUID id,
+												   @RequestHeader(value = "Accept-Language", defaultValue = "en", required = false) String locale){
+		return ResponseEntity.ok(service.findById(id, new Locale(locale)));
+	}
+	
+	@PutMapping(value = "/update/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<CpfDTOResponse> update(@PathVariable UUID id,
+												  @RequestBody @Valid CpfDTORequest dto,
+												  @RequestHeader(value = "Accept-Language", defaultValue = "en", required = false) String locale){
+		return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON)
+					.body(service.update(id, dto, new Locale(locale)));
+	}
+	
+	@DeleteMapping("/{physicalPersonId}")
+    public ResponseEntity<String> deletePhysicalPerson(@PathVariable UUID physicalPersonId) {
+        Locale locale = Locale.getDefault();
+
+        service.deletePhysicalPerson(physicalPersonId, locale);
+
+        String message = String.format("PhysicalPerson com ID %s foram excluídos com sucesso.", physicalPersonId);
+        return ResponseEntity.ok(message);
+    }
+	
+	@GetMapping("/cpf")
+	public ResponseEntity<CpfDTOResponse> findByCpf(@RequestParam("cpf") String cpf, Locale locale) {
+		CpfDTOResponse response = service.findByCpf(cpf, locale);
+	    return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/search")
+    public ResponseEntity<List<CpfDTOResponse>> findByNameStartingWithIgnoreCase(@RequestParam("name") String name,     		
+					  															 @RequestHeader(value = "Accept-Language", defaultValue = "en", required = false) String locale) {
+         List<CpfDTOResponse> legalPersons = service.findByNameStartingWithIgnoreCase(name, new Locale(locale));
+        return ResponseEntity.ok(legalPersons);
     }
 
 }
--- src/main/java/com/portifolio/imobiliaria/service/PhysicalPersonServiceImpl.java
+++ src/main/java/com/portifolio/imobiliaria/service/PhysicalPersonServiceImpl.java
@@ -2,9 +2,12 @@ package com.portifolio.imobiliaria.service;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.context.MessageSource;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,8 @@ import com.portifolio.imobiliaria.entities.PhysicalPerson;
 import com.portifolio.imobiliaria.exception.InvalidDocumentException;
 import com.portifolio.imobiliaria.repositories.PhysicalPersonRepository;
 import com.portifolio.imobiliaria.valid.ValidationCPF_CNPJ;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PhysicalPersonServiceImpl implements PhysicalPersonService{
@@ -57,32 +62,97 @@ public class PhysicalPersonServiceImpl implements PhysicalPersonService{
 
 	@Override
 	public CpfDTOResponse findById(UUID id, Locale locale) {
-		// TODO Auto-generated method stub
-		return null;
+		PhysicalPerson physicalPerson = physicalPersonById(id, locale);
+		CpfDTOResponse cpfDTOResponse = PhysicalPersonMapper.cpfFromPhysicalPersonEntity(physicalPerson);
+		return cpfDTOResponse;
+	}
+	
+	private PhysicalPerson physicalPersonById(UUID id, Locale locale) {
+		Optional<PhysicalPerson> optional = repository.findById(id);
+		if(optional.isEmpty()) {
+			throw new EntityNotFoundException(
+					String.format(messages.getMessage("person.message.error-non-existent-entity-by-id", null, locale), id)
+					);
+		}
+		return optional.get();
 	}
 
 	@Override
 	public CpfDTOResponse update(UUID id, CpfDTORequest dto, Locale locale) {
-		// TODO Auto-generated method stub
-		return null;
+		PhysicalPerson physicalPerson = physicalPersonById(id, locale);
+		physicalPersonValidatedName(dto.getName(), locale);
+		physicalPersonValidatedCPF(dto.getCpf(), locale);
+		ValidationCPF_CNPJ validationCPF_CNPJ = new ValidationCPF_CNPJ();
+		 if (!validationCPF_CNPJ.isValidCPF(dto.getCpf())) {
+		        throw new InvalidDocumentException(
+		                String.format(messages.getMessage("person.message.error-ivalid-cpf", null, locale))
+		        );
+		    }
+		physicalPerson.setCpf(dto.getCpf());
+		physicalPerson.setName(dto.getName());
+		
+		PhysicalPerson savedPhysicalPerson = repository.save(physicalPerson);
+		CpfDTOResponse cpfDTOResponse = PhysicalPersonMapper.cpfFromPhysicalPersonEntity(savedPhysicalPerson);
+		
+		return cpfDTOResponse;
+	}
+	
+	private void physicalPersonValidatedName(String name, Locale locale) {
+		if(repository.findByNameIgnoreCase(name).isPresent()) {
+			throw new DataIntegrityViolationException(
+					String.format(messages.getMessage("person.message.error-already-registered", null, locale))
+					);
+		}
+		new PhysicalPerson();
+	}
+	
+	private void physicalPersonValidatedCPF(String cpf, Locale locale) {
+		if(repository.findByCpf(cpf).isPresent()) {
+			throw new DataIntegrityViolationException(
+					String.format(messages.getMessage("person.message.error-cpf-already-registered", null, locale))
+					);
+		}
+		new PhysicalPerson();
 	}
 
 	@Override
 	public void deletePhysicalPerson(UUID id, Locale locale) {
-		// TODO Auto-generated method stub
+		PhysicalPerson physicalPerson = physicalPersonById(id, locale);
+		repository.delete(physicalPerson);
 		
 	}
 
 	@Override
 	public CpfDTOResponse findByCpf(String cpf, Locale locale) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<PhysicalPerson> optional = repository.findByCpf(cpf);
+		
+		if (optional.isEmpty()) {
+	        throw new EntityNotFoundException(
+	            String.format(messages.getMessage("physicalperson.message.error-not-found-cpf", null, locale), cpf)
+	        );
+	    }
+		
+		PhysicalPerson physicalPerson = optional.get();
+		CpfDTOResponse cpfDTOResponse = PhysicalPersonMapper.cpfFromPhysicalPersonEntity(physicalPerson);
+		
+		return cpfDTOResponse;
 	}
 
 	@Override
 	public List<CpfDTOResponse> findByNameStartingWithIgnoreCase(String name, Locale locale) {
-		// TODO Auto-generated method stub
-		return null;
+		List<PhysicalPerson> physicalPerson = repository.findByNameStartingWithIgnoreCase(name);
+		
+		return physicalPerson.stream()
+				.map(this::mapPhysicalPersonToCpfDTOResponse)
+				.collect(Collectors.toList());
+	}
+	
+	private CpfDTOResponse mapPhysicalPersonToCpfDTOResponse(PhysicalPerson physicalPerson) {
+		CpfDTOResponse cpfDTOResponse = new CpfDTOResponse();
+		cpfDTOResponse.setId(physicalPerson.getId());
+		cpfDTOResponse.setCpf(physicalPerson.getCpf());
+		cpfDTOResponse.setName(physicalPerson.getName());
+		return cpfDTOResponse;
 	}
 
 }
